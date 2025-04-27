eea8fd01c40c99069f61a6e2eebbd638ce55b33e
Flor Stellamans <flor.stellamans@student.howest.be>
2025-04-27 09:27:36 +0200

    add expense endpoints

--- src/main/java/org/acme/controller/ExpenseController.java
+++ src/main/java/org/acme/controller/ExpenseController.java
@@ -6,8 +6,11 @@ import org.acme.domain.Expense;
 import org.acme.service.ExpenseService;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -17,9 +20,27 @@ public class ExpenseController {
     @Inject
     ExpenseService expenseService;
 
+    // add parameter filters
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public List<Expense> getAllExpenses() {
         return expenseService.getAllExpenses();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Expense getExpenseById(@PathParam("id") int id) {
+        return expenseService.getExpenseById(id);
+    }
+
+    @POST
+    public String addExpense() {
+        return expenseService.addExpense();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public String removeExpense(@PathParam("id") int id) {
+        return expenseService.removeExpense(id);
     }
 }
--- src/main/java/org/acme/service/ExpenseService.java
+++ src/main/java/org/acme/service/ExpenseService.java
@@ -15,4 +15,23 @@ public class ExpenseService {
     public List<Expense> getAllExpenses() {
         return expenses;
     }
+
+    public Expense getExpenseById(int id) {
+        return expenses.get(id);
+    }
+
+    public String addExpense() {
+        // try catch
+        //get body data
+        Expense expense = new Expense(0, 0, null, null, null);
+        expenses.add(expense);
+        return "Expense added";
+    }
+
+    public String removeExpense(int id) {
+        // try catch
+        Expense expense = getExpenseById(id);
+        expenses.remove(expense);
+        return "Expense removed";
+    }
 }
